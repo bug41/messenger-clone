fe27c88fee757b78c90def5ccae0910165e87659
bug41 <bugkinguswit@gmail.com>
2023-05-29 17:42:27 +0900

    소셜 로그인 구현중

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import axios from "axios";
 import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
 import { useCallback, useState } from "react";
@@ -8,6 +9,9 @@ import { FieldValues, useForm, SubmitHandler } from "react-hook-form";
 import {BsGithub, BsGoogle} from 'react-icons/bs';
 
 import AuthSocialButton from "./AuthSocialButton";
+import {toast} from "react-hot-toast";
+import {signIn} from "next-auth/react";
+
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -44,18 +48,43 @@ const AuthForm = () => {
         setIsLoading(true);
         
         if(variant == 'REGISTER'){
-            //Axios Register
+           axios.post('/api/register',data)
+           .catch(() => toast.error('Something went wrong!'))
+           .finally(() => setIsLoading(false))
         }
 
         if(variant == 'LOGIN'){
-            //NextAuth SignIn
+            signIn('credentials', {
+                ...data,
+                redirect: false
+            })
+            .then((callback) => {
+                if(callback?.error) {
+                    toast.error('Invalid credentials');
+                }
+
+                if(callback?.ok && !callback?.error){
+                    toast.success('Logged in!')
+                }
+            })
+            .finally(() => setIsLoading(false));
         }
     }
 
     const socialAction = (action: string) => {
         setIsLoading(true);
 
-        //NextAuth Social Sign In
+        signIn(action, {redirect:false})
+        .then((callback) => {
+            if(callback?.error){
+                toast.error('Invalid Credentials')
+            }
+
+            if(callback?.ok && !callback?.error){
+                toast.success('Logged in!')
+            }
+        })
+        .finally(() => setIsLoading(false));
     }
 
     return (
