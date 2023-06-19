f753fda70e25db247555d64a44efc77c0c289be5
bugkingus <bugkingus@gmail.com>
2023-06-19 09:06:21 +0900

    프로필 이미지 변경건 추가

--- app/components/sidebar/SettingsModal.tsx
+++ app/components/sidebar/SettingsModal.tsx
@@ -7,6 +7,10 @@ import { useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import Modal from "../Modal";
+import Input from "../inputs/Input";
+import Image from "next/image";
+import { CldUploadButton } from "next-cloudinary";
+import Button from "../Button";
 
 interface SettingsModalProps{
     isOpen?: boolean;
@@ -48,6 +52,7 @@ const SettingsModal: React.FC<SettingsModalProps>= ({
 
     const onSubmit: SubmitHandler<FieldValues> = (data) => {
         setIsLoading(true);
+        
         axios.post('/api/settings', data)
         .then(() => {
             router.refresh();
@@ -62,9 +67,57 @@ const SettingsModal: React.FC<SettingsModalProps>= ({
             isOpen={isOpen}
             onClose={onClose}>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    
+                    <div className="space-y-12">
+                        <div className="border-b border-gray-900/10 pb-12">
+                            <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
+                            <p className="mt-1 text-sm leading-6 text-gray-600">Edit your public information</p>
+                            <div className="mt-10 flex flex-col gap-y-8">
+                                <Input
+                                    disabled={isLoading}
+                                    label="Name"
+                                    id="name"
+                                    errors={errors}
+                                    required
+                                    register={register}
+                                />
+                                <div>
+                                    <label className="block text-sm font-medium leading-6 text-gray-900">
+                                        Photo
+                                    </label>
+                                    <div className="mt-2 flex items-center gap-x-3 ">
+                                        <Image 
+                                            width="48" height="48" className="rounded-full"
+                                            src={image || currentUser?.image || '/images/placeholder.jpg'}
+                                            alt="Avatar"
+                                        />
+                                         <CldUploadButton
+                                            options={{ maxFiles:1 }}
+                                            onUpload={handleUpload}
+                                            uploadPreset="fqbmozgh"
+                                            >
+                                                <Button
+                                                    disabled={isLoading}
+                                                    secondary
+                                                    type="button"
+                                                >
+                                                    Change
+                                                </Button>
+                                        </CldUploadButton>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div
+                            className="mt-6 flex items-center justify-end gap-x-6"
+                        >
+                            <Button disabled={isLoading} secondary onClick={onClose}>Cancel</Button>
+                            <Button disabled={isLoading} type="submit">Save</Button>
+                        </div>
+
+                    </div>
                 </form>
-            </Modal>
+        </Modal>
     );
 }
  
