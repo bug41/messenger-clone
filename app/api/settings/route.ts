7681e586c692a14868ea9f37a1e1c609e826ae38
bug41 <bugkinguswit@gmail.com>
2023-06-21 00:04:29 +0900

    사용자 프로필 수정

--- app/api/settings/route.ts
+++ app/api/settings/route.ts
@@ -18,6 +18,17 @@ export async function POST(
             return new NextResponse('Unauthorized', {status:401});
         }
 
+        const updatedUser = await prisma.user.update({
+            where: {
+                id: currentUser.id
+            },
+            data: {
+                image: image,
+                name: name
+            }
+        });
+
+        return NextResponse.json(updatedUser);
     }catch(error: any){
         console.log(error, 'ERROR_SETTINGS');
         return new NextResponse('Internal Error', {status:500});
